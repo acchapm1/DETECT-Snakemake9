badd07b263cce1ae0b33e9edf448a6233bf514e4
Alan Chapman <acchapm1@asu.edu>
2025-11-06 08:36:00 -0700

    adding scripts to demo/

---
+++ demo/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load mamba/latest
+# source activate sn9detect
+source activate DETECT
+
+BASE="$(pwd)"
+
+
+echo "Config created Successfully"
+echo " "
+echo "Starting DETECT run"
+echo " "
+
+snakemake -p \
+  --profile profiles/slurm \
+  --configfile data/config/config.json \
+  -s DETECT/Snakefile
---
+++ demo/sbatch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH -t 3-00:00:00
+#SBATCH -p public
+#SBATCH -q public
+#SBATCH --job-name=detect_sn9
+#SBATCH -o logs/o-%j.out
+#SBATCH -e logs/e-%j.err
+
+module load mamba/latest
+# source activate sn9detect
+source activate DETECT
+
+BASE="$(pwd)"
+
+
+echo "Config created Successfully"
+echo " "
+echo "Starting DETECT run"
+echo " "
+
+snakemake -p \
+  --profile profiles/slurm \
+  --configfile data/config/config.json \
+  -s DETECT/Snakefile
